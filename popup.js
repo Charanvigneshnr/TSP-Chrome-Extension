3c31d1a8664ea01203c7ea0a3e0f9de70082cfc6
Charanvigneshnr <charanvignesh10@gmail.com>
2023-04-22 14:50:56 +0530

    js file updated

--- popup.js
+++ popup.js
@@ -1,144 +1,104 @@
-// When the popup HTML has loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the "Calculate Route" button
-    var calculateRouteButton = document.getElementById('calculate-route-button');
+// Define global variables
+let map, service, directionsRenderer;
 
-    // When the "Calculate Route" button is clicked
-    calculateRouteButton.addEventListener('click', function() {
-        // Get the item the user wants to buy
-        var item = document.getElementById('item-input').value;
-        
-        // Get the number of shops the user can visit
-        var numShops = document.getElementById('num-shops-input').value;
-
-        // Calculate the shortest route to visit all the shops
-        calculateShortestRoute(item, numShops);
-    });
+// Initialize the map on popup load
+document.addEventListener("DOMContentLoaded", () => {
+  initMap();
 });
 
-function calculateShortestRoute(item, numShops) {
-    // Get the user's current location
-    navigator.geolocation.getCurrentPosition(function(position) {
-        // Set up the Google Maps Directions Service
-        var directionsService = new google.maps.DirectionsService();
-        
-        // Set up the map
-        var map = new google.maps.Map(document.createElement('div'));
-        
-        // Set up the directions renderer
-        var directionsDisplay = new google.maps.DirectionsRenderer({ map: map });
-        
-        // Set up variables to store the distances to each shop
-        var distances = [];
-        var numRequests = 0;
-        
-        // Make a request to the Directions Service for each shop location
-        for (var i = 0; i < numShops; i++) {
-            var shopName = "Shop " + (i+1);
-            var shopAddress = "123 Shop St.";
-            
-            // Replace with your own code to get shop names and addresses
-            // from a database or API based on the item the user wants to buy
-            
-            var request = {
-                origin: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-                destination: shopAddress,
-                travelMode: google.maps.TravelMode.DRIVING
-            };
-            
-            directionsService.route(request, function(result, status) {
-                if (status == google.maps.DirectionsStatus.OK) {
-                    var route = result.routes[0];
-                    var distance = 0;
-                    for (var i = 0; i < route.legs.length; i++) {
-                        distance += route.legs[i].distance.value;
-                    }
-                    console.log("Distance to " + shopName + " (" + shopAddress + "): " + distance + " meters");
-                    
-                    // Store the distance value in the distances array
-                    distances.push(distance);
-                    
-                    // Increment the number of requests completed
-                    numRequests++;
-                    
-                    // If all requests have completed
-                    if (numRequests == numShops) {
-                        // Find the shortest path using the distances array
-                        var shortestPath = findShortestPath(distances);
-                        
-                        // Update the UI with the shortest path
-                        var shortestPathDiv = document.getElementById('shortest-path');
-                        shortestPathDiv.innerHTML = shortestPath;
-                    }
-                }
-            });
-        }
+function initMap() {
+  // Get the user's location
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    const center = new google.maps.LatLng(latitude, longitude);
+
+    // Create the map centered on the user's location
+    map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 12,
+      center: center,
     });
+
+    // Create a DirectionsRenderer object to display the directions
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+    directionsRenderer.setPanel(document.getElementById("directions"));
+
+    // Create a PlacesService object to search for nearby places
+    service = new google.maps.places.PlacesService(map);
+
+    // Add a click listener to the search button
+    const searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener("click", handleSearchClick);
+  });
 }
 
-function findShortestPath(distances) {
-    // Generate all permutations of the distances array
-    var permutations = permute(distances);
-    
-    // Set up variables to store the shortest path and distance
-    var shortestPath;
-    var shortestDistance = Number.MAX_VALUE;
-    
-    // Iterate through each permutation
-    for (var i = 0; i < permutations.length; i++) {
-        // Calculate the total distance of the current permutation
-        var totalDistance = 0;
-        for (var j = 1; j < permutations[i].length; j++) {
-            totalDistance += permutations[i][j];
-        }
-        if (totalDistance < shortestDistance) {
-            shortestDistance = totalDistance;
-            shortestPath = permutations[i];
-            }
-            }// Return the shortest path
-return shortestPath.join(" -> ");
-}
+function handleSearchClick() {
+  // Get the user's input values
+  const searchInput = document.getElementById("searchInput");
+  const query = searchInput.value;
+  const numShopsInput = document.getElementById("numShopsInput");
+  const numShops = numShopsInput.value;
 
-function permute(array) {
-// If the array is empty, return an empty array
-if (array.length == 0) {
-return [[]];
-}// Get the first element of the array
-var element = array[0];
+  // Call the PlacesService nearbySearch method to find nearby places
+  service.nearbySearch(
+    {
+      location: map.getCenter(),
+      radius: 10000,
+      type: "store",
+      keyword: query,
+    },
+    (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        // Sort the results by distance from the center of the map
+        results.sort((a, b) => {
+          const distA = google.maps.geometry.spherical.computeDistanceBetween(
+            map.getCenter(),
+            a.geometry.location
+          );
+          const distB = google.maps.geometry.spherical.computeDistanceBetween(
+            map.getCenter(),
+            b.geometry.location
+          );
+          return distA - distB;
+        });
 
-// Get all permutations of the remaining array elements
-var permutations = permute(array.slice(1));
+        // Limit the number of results to the user's input value
+        const limitedResults = results.slice(0, numShops);
 
-// Set up an array to store the new permutations
-var newPermutations = [];
+        // Create an array of LatLng objects representing the locations of the places
+        const locations = limitedResults.map((result) => result.geometry.location);
 
-// Iterate through each existing permutation
-for (var i = 0; i < permutations.length; i++) {
-    // Iterate through each position in the permutation
-    for (var j = 0; j <= permutations[i].length; j++) {
-        // Insert the current element at the current position
-        var permutation = permutations[i].slice();
-        permutation.splice(j, 0, element);
-        
-        // Add the new permutation to the array
-        newPermutations.push(permutation);
+        // Use the Traveling Salesman Problem algorithm to find the shortest route between the locations
+        const tsp = new TravelingSalesmanProblem();
+        const solution = tsp.solve(locations);
+
+        // Create a DirectionsRequest object to get directions between the locations
+        const waypoints = solution.map((location) => {
+          return {
+            location: location,
+            stopover: true,
+          };
+        });
+
+        const origin = waypoints.shift().location;
+        const destination = waypoints.pop().location;
+
+        const request = {
+          origin: origin,
+          destination: destination,
+          waypoints: waypoints,
+          optimizeWaypoints: true,
+          travelMode: google.maps.TravelMode.DRIVING,
+        };
+
+        // Call the DirectionsService route method to get the directions
+        const directionsService = new google.maps.DirectionsService();
+        directionsService.route(request, (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+          }
+        });
+      }
     }
+  );
 }
-
-// Return the new permutations
-return newPermutations;
-}
-// Get the user inputs
-var item = document.getElementById("item").value;
-var numShops = parseInt(document.getElementById("num-shops").value);
-// Display the result on the HTML page
-document.getElementById("result").innerHTML = "To buy " + item + " at " + numShops + " shops, visit the shops in the following order: " + result;
-// Get the user inputs
-var item = document.getElementById("item").value;
-var numShops = parseInt(document.getElementById("num-shops").value);
-
-// Solve the TSP problem
-var result = solveTSP(numShops);
-
-// Display the result on the HTML page
-document.getElementById("result").innerHTML = "To buy " + item + " at " + numShops + " shops, visit the shops in the following order: " + result;
